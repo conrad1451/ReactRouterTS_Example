9da1043d8174dd5e610577b7adfa064685b93cdf
Conrad Hansen-Quartey <Chansenq@gmail.com>
2024-01-12 12:38:58 -0500

    Update to get Home working

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 // import { About } from "./pages/About";
 // import { Contact } from "./pages/Contact";
-import { Home } from "./pages/Home";
+// import { Home } from "./pages/Home";
+import Home from "./pages/Home"
 // import { Portfolio } from "./pages/Portfolio";
 
 function App() {
